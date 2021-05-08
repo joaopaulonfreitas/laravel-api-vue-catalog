eb25afb1d51d0789c196f50ac0eb7f6ae40df8ec
João Paulo N. Freias <joaopaulonfreitas@gmail.com>
2021-05-08 14:05:35 -0300

    [setup] Initial Installation of project. add laravel ui, vue, browserSync and SQLite database

---
+++ public/js/app.js
@@ -0,0 +1,24 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./resources/js/app.js":
+/*!*****************************!*\
+  !*** ./resources/js/app.js ***!
+  \*****************************/
+/***/ (() => {
+
+throw new Error("Module build failed (from ./node_modules/babel-loader/lib/index.js):\nError: [BABEL] /home/joaopaulo/www/api-vue-catalog/resources/js/app.js: Cannot find module '@babel/parser'\nRequire stack:\n- /home/joaopaulo/www/api-vue-catalog/node_modules/@babel/core/lib/index.js\n- /home/joaopaulo/www/api-vue-catalog/node_modules/laravel-mix/src/FileCollection.js\n- /home/joaopaulo/www/api-vue-catalog/node_modules/laravel-mix/src/tasks/ConcatenateFilesTask.js\n- /home/joaopaulo/www/api-vue-catalog/node_modules/laravel-mix/src/components/Combine.js\n- /home/joaopaulo/www/api-vue-catalog/node_modules/laravel-mix/src/components/ComponentRegistrar.js\n- /home/joaopaulo/www/api-vue-catalog/node_modules/laravel-mix/src/Mix.js\n- /home/joaopaulo/www/api-vue-catalog/node_modules/laravel-mix/setup/webpack.config.js\n- /home/joaopaulo/www/api-vue-catalog/node_modules/webpack-cli/lib/webpack-cli.js\n- /home/joaopaulo/www/api-vue-catalog/node_modules/webpack-cli/lib/bootstrap.js\n- /home/joaopaulo/www/api-vue-catalog/node_modules/webpack-cli/bin/cli.js\n- /home/joaopaulo/www/api-vue-catalog/node_modules/webpack/bin/webpack.js\n    at Function.Module._resolveFilename (node:internal/modules/cjs/loader:941:15)\n    at Function.Module._load (node:internal/modules/cjs/loader:774:27)\n    at Module.require (node:internal/modules/cjs/loader:1013:19)\n    at require (/home/joaopaulo/www/api-vue-catalog/node_modules/v8-compile-cache/v8-compile-cache.js:159:20)\n    at _parser (/home/joaopaulo/www/api-vue-catalog/node_modules/@babel/core/lib/index.js:209:16)\n    at Object.get [as tokTypes] (/home/joaopaulo/www/api-vue-catalog/node_modules/@babel/core/lib/index.js:40:12)\n    at Function.assign (<anonymous>)\n    at /home/joaopaulo/www/api-vue-catalog/node_modules/@babel/core/lib/config/full.js:210:24\n    at Generator.next (<anonymous>)\n    at Function.<anonymous> (/home/joaopaulo/www/api-vue-catalog/node_modules/@babel/core/lib/gensync-utils/async.js:16:3)");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./resources/js/app.js"]();
+/******/ 	
+/******/ })()
+;
