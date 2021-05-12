c857ee55f29c632b2fab8fb6f99ae498db9de531
João Paulo N. Freias <joaopaulonfreitas@gmail.com>
2021-05-12 14:06:02 -0300

    [notification] send mail using notify class

---
+++ app/Notifications/WelcomeEmailNotification.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class WelcomeEmailNotification extends Notification
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+                    ->subject('Seja Bem Vindo(a) '.$notifiable->name)
+                    ->greeting('Olá Sr.(a) '.$notifiable->name)
+                    ->line('Seja muito bem vindo(a) a nossa plataforma')
+                    ->action('Acessar Site Agora!', url('/'))
+                    ->line('Obrigado por usar nossa aplicação!');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
